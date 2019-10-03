3b36dd0c6da667b1ee46ba9cc7bef074578a4a61
Dejan Trajkovic <dtrajko@gmail.com>
2019-10-04 00:27:15 +0200

    Section 2: Stealth Game - 14. Setup Objective UI with UMG

--- Source/FPSGame/Public/FPSCharacter.h
+++ Source/FPSGame/Public/FPSCharacter.h
@@ -48,6 +48,9 @@ public:
 	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
 	UAnimSequence* FireAnimation;
 
+	UPROPERTY(BlueprintReadOnly, Category = "Gameplay")
+	bool bIsCarryingObjective;
+
 protected:
 	
 	/** Fires a projectile. */
@@ -69,4 +72,3 @@ public:
 	UCameraComponent* GetFirstPersonCameraComponent() const { return CameraComponent; }
 
 };
-
