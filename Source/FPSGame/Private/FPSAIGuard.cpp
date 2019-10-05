6abfd55b24b41eeb947c0e8b4031aa31a90896e6
Dejan Trajkovic <dtrajko@gmail.com>
2019-10-05 05:43:52 +0200

    Section : tealth ame  4. Create AI Guard Class (done)

--- Source/FPSGame/Private/FPSAIGuard.cpp
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -2,6 +2,8 @@
 
 
 #include "FPSAIGuard.h"
+#include "Perception/PawnSensingComponent.h"
+
 
 // Sets default values
 AFPSAIGuard::AFPSAIGuard()
@@ -9,6 +11,7 @@ AFPSAIGuard::AFPSAIGuard()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
 }
 
 // Called when the game starts or when spawned
@@ -24,11 +27,3 @@ void AFPSAIGuard::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
-// Called to bind functionality to input
-void AFPSAIGuard::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
