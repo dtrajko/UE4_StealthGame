67764403f65e1f3bdca8e38e0addbb250ca1f3b9
Dejan Trajkovic <dtrajko@gmail.com>
2019-10-06 23:12:26 +0200

    Section 5: Stealth Game - 40. Network the Game State Part 3

--- Source/FPSGame/Private/FPSGameMode.cpp
+++ Source/FPSGame/Private/FPSGameMode.cpp
@@ -33,10 +33,15 @@ void AFPSGameMode::CompleteMission(APawn* InstigatorPawn, bool bMissionSuccess)
 			if (ReturnedActors.Num() > 0)
 			{
 				AActor* NewViewTarget = ReturnedActors[0];
-				APlayerController* PC = Cast<APlayerController>(InstigatorPawn->GetController());
-				if (PC)
+
+				FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator();
+				for (It; It; It++)
 				{
-					PC->SetViewTargetWithBlend(NewViewTarget, 1.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+					APlayerController* PC = It->Get();
+					if (PC)
+					{
+						PC->SetViewTargetWithBlend(NewViewTarget, 1.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+					}
 				}
 			}
 		}
--- Source/FPSGame/Private/FPSGameState.cpp
+++ Source/FPSGame/Private/FPSGameState.cpp
@@ -2,17 +2,35 @@
 
 
 #include "FPSGameState.h"
+#include "FPSPlayerController.h"
 
 
 void AFPSGameState::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bMissionSuccess)
 {
-	FConstPawnIterator It = GetWorld()->GetPawnIterator();
+	// FConstPawnIterator It = GetWorld()->GetPawnIterator();
+	// for (It; It; It++)
+	// {
+	// 	APawn* Pawn = It->Get();
+	// 	if (Pawn && Pawn->IsLocallyControlled())
+	// 	{
+	// 		Pawn->DisableInput(nullptr);
+	// 	}
+	// }
+
+	FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator();
 	for (It; It; It++)
 	{
-		APawn* Pawn = It->Get();
-		if (Pawn && Pawn->IsLocallyControlled())
+		AFPSPlayerController* PC = Cast<AFPSPlayerController>(It->Get());
+		if (PC && PC->IsLocalController())
 		{
-			Pawn->DisableInput(nullptr);
+			PC->OnMissionCompleted(InstigatorPawn, bMissionSuccess);
+
+			// Disable input
+			APawn* MyPawn = PC->GetPawn();
+			if (MyPawn)
+			{
+				MyPawn->DisableInput(PC);
+			}
 		}
 	}
 }
